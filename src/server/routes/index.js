d7c348fa6cfc63a39ba31a77b44bd8ceb30bb499
daleroy <david.a.leroy@gmail.com>
2016-06-19 18:49:46 +0000

    Hookup sendmail for contact form.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var path = require('path');
 var nodemailer = require('nodemailer');
+var sendmailTransport = require('nodemailer-sendmail-transport');
 
 var routes = express();
+var options = {
+    path: '/usr/lib/sendmail'
+}
+
+var transporter = nodemailer.createTransport(sendmailTransport(options))
 
 routes.use(function(req, res, next) {
-    console.log('Request for AFrame received.');
-    console.log('REQ: ' + req.url);
+    console.log('----- Request for AFrame received.-----');
+    console.log('req.url: ' + req.url);
+    console.log('req.baseUrl: ' + req.baseUrl);
+    console.log('req.method: ' + req.method);
+    console.log('req.originalUrl: ' + req.originalUrl);
+    console.log('req.hostname: ' + req.hostname);
+    console.log('req.params: ' + JSON.stringify(req.params));
+    console.log('req.path' + req.path);
+    console.log('---------------------------------------');
+    next();
 });
 
 routes.post('/mail', function(req, res, next) {
-    console.log('MAIL endpoint');
+    console.log('Attempting to send email.');
     var mailOptions = {
         from: '"A-Frame Website" contactform@aframe.com',
         to: 'david.a.leroy@gmail.com',
@@ -21,6 +35,7 @@ routes.post('/mail', function(req, res, next) {
     transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
             res.send(500);
+            console.log('Message failed.');
             return console.log(error);
         }
         console.log('Message sent: ' + info.response);
@@ -28,10 +43,5 @@ routes.post('/mail', function(req, res, next) {
         res.send(200);
     }); 
 });
-/*
-routes.get('/', function(req, res) {
-    console.log('Request received for AFrame');
-    res.send('Aframe');
-});
-*/
+
 module.exports = routes;
